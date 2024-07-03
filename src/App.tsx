fb2b6461905c50edd3e1558e4bb6c8c1377e1006
sana-lazystar <sana.lazystar@gmail.com>
2024-07-03 12:31:44 +0900

    # Remove unused files and setting lint & formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Suspense, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import '@/App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const healthCheck = async () => {
-    const HEALTH_URL = "http://localhost/health";
+    const HEALTH_URL = 'http://localhost/health';
 
     try {
       const response = await fetch(HEALTH_URL);
 
-      if (!response.ok) throw new Error("Health check failed!");
+      if (!response.ok) throw new Error('Health check failed!');
 
       const responseJSON = await response.json();
 
-      console.log("Health check passed!", responseJSON);
+      console.log('Health check passed!', responseJSON);
     } catch (error) {
       console.error(error);
     }
@@ -26,29 +24,13 @@ function App() {
     healthCheck();
   }, []);
 
+  // TODO: Make fallback to Suspense component.
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Suspense>
+      <Routes>
+        <Route path={'/home'} element={<div>Home</div>} />
+      </Routes>
+    </Suspense>
   );
 }
 
